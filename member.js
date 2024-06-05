50a9169f925600884af624269dc4c51c5ae82982
jgiordmaina <100682990+jgiordmaina@users.noreply.github.com>
2024-06-05 21:06:30 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,1 +1,7 @@
-function skillsMember() { 
+function skillsMember() {        
+    var member = new Object();
+    member.name = "Jhon";
+    member.age = 30;
+    member.skills = ["C#", "Java", "Python"];
+    return member;
+}
